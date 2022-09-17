a8cd1e00dd06f98b443c500473d2e6403fd42e00
Jose David <jose7312000@gmail.com>
2022-09-16 21:12:15 -0500

    Agregando hoisting

---
+++ src/hoisting/hoisting.js
@@ -0,0 +1,12 @@
+
+//Cuando llamamos una funcion y una variable antes de su declaracion
+nameOfDog();
+
+function nameOfDog(){
+    console.log(`El mejor perro es ${namePerro}`);
+}
+
+// Pero si agregamos o definimos una varible si se ejecuta el codigo
+var namePerro;
+
+// "El mejor perro es _undefined_"
